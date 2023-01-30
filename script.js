b7095565cc2b9daf0799ee37e680c9cd7fe0d0af
ehsanm <ehsanmohamadi780@gmail.com>
2023-01-30 20:05:19 +0330

    add new trick to  Q3

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 "use strict";
 
-// Trick 1
-function unique(arr) {
-  let result = [];
-  for (let item of arr) {
-    if (!result.includes(item)) {
-      result.push(item);
-    }
-  }
-  return result;
-}
 let values = [
   "Hare",
   "Krishna",
@@ -21,4 +11,20 @@ let values = [
   "Hare",
   ":-O",
 ];
+// Trick 1
+function unique(arr) {
+  let result = [];
+  for (let item of arr) {
+    if (!result.includes(item)) {
+      result.push(item);
+    }
+  }
+  return result.join(" , ");
+}
 console.log(unique(values));
+
+// Trick 2
+// function unique(arr) {
+//   return Array.from(new Set(arr));
+// }
+// console.log(unique(values).join(" , "));
