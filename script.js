9107e30175169dd0835a33bb54ee322b33466d72
ehsanm <ehsanmohamadi780@gmail.com>
2023-02-01 23:53:07 +0330

    final answer Q6

--- script.js
+++ script.js
@@ -2,24 +2,25 @@ let btn = document.getElementById("btn");
 
 function validateForm(e) {
   e.preventDefault();
-  let firstName = document.getElementById("firstName").value;
-  let middleName = document.getElementById("middleName").value;
-  let lastName = document.getElementById("lastName").value;
+  let firstName = document.getElementById("firstName").value.trim();
+  let middleName = document.getElementById("middleName").value.trim();
+  let lastName = document.getElementById("lastName").value.trim();
   let selectOption = document.getElementById("selectOption").value;
   let maleRadio = document.getElementById("male");
   let femaleRadio = document.getElementById("female");
   let otherRadio = document.getElementById("other");
   let selectedGender;
   console.log(gender);
-  let phoneNumber = document.getElementById("phone").value;
-  let phoneCode = document.getElementById("phone-code").value;
+  let phoneNumber = document.getElementById("phone").value.trim();
+  let phoneCode = document.getElementById("phone-code").value.trim();
   let textArea = document.getElementById("textarea").value;
-  let email = document.getElementById("email").value;
-  let password = document.getElementById("password").value;
+  let email = document.getElementById("email").value.trim();
+  let password = document.getElementById("password").value.trim();
   let rePassword = document.getElementById("rePassword").value;
   let emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-
-  if (firstName === "" || middleName === "" || lastName === "") {
+  let passwordPattern =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  if (firstName === "" || middleName === "" || lastName === "" || undefined) {
     alert("Name fields cannot be empty");
     return false;
   }
@@ -28,6 +29,7 @@ function validateForm(e) {
     alert("Please select an option");
     return false;
   }
+
   if (maleRadio.checked) {
     selectedGender = "male";
   } else if (femaleRadio.checked) {
@@ -75,12 +77,26 @@ function validateForm(e) {
     alert("Password fields cannot be empty");
     return false;
   }
+  if (!passwordPattern.test(password)) {
+    alert(
+      "Invalid password. Password must contain at least 8 characters, one lowercase letter, one uppercase letter, one number, and one special character."
+    );
+    return false;
+  }
   if (password !== rePassword) {
     alert("Passwords do not match");
     return false;
   }
+
   let obj = {
     "First name": firstName,
+    "Middle Name": middleName,
+    "last name": lastName,
+    Course: selectOption,
+    Gender: selectedGender,
+    "Phone Number": phoneNumber,
+    Email: email,
+    Pasword: password,
   };
   console.log(obj);
   // form.reset();
